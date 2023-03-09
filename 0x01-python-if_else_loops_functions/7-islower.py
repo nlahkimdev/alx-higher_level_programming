0c983f90913c8379ab9cea77c77c76b6891c62ee
nlahkimdev <nlahkimdev@gmail.com>
2023-03-09 12:47:19 -0800

    fix 2:isLower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if c <= chr(90):
+    if c <= chr(90) and c <= chr(48):
         return False
-    elif c >= chr(97):
+    elif c >= chr(97) and c <= chr(122):
         return True
