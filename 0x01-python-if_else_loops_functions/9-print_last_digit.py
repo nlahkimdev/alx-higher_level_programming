38ddb820b082d06a23c5218a4071a67c47b2c8d0
nlahkimdev <nlahkimdev@gmail.com>
2023-03-09 14:00:05 -0800

    fix :last digity

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     numToStr = str(number)
-    print(numToStr[-1])
+    last = numToStr[-1]
+    if number < 0:
+        result = int(last) * -1
+    else:
+        result = int(last)
+    print("{:d}".format(result))
