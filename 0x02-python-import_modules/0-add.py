798001f6d3cf069bdf79df0a9afb3759ac091477
nlahkimdev <nlahkimdev@gmail.com>
2023-03-09 16:55:03 -0800

    fix 5:import function

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+import sys
+import os
+
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
 if __name__ == "__main__":
     import add_0
-
     a = 1
     b = 2
     print("{:d} + {:d} = {:d}".format(a, b, add_0.add(a, b)))
