a6fa50522c524806e132cf136f68f2deda3f74fb
nlahkimdev <nlahkimdev@gmail.com>
2023-03-09 17:43:43 -0800

    fix :dyncamic

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,9 @@ if __name__ == "__main__":
     ln = len(sys.argv)
     if ln == 1:
         print("{} arguments.".format(ln - 1))
+    elif ln == 2:
+        print("{} argument:".format(ln - 1))
+        print("{}: {}".format(1, sys.argv[1]))
     else:
         print("{} arguments:".format(ln - 1))
         for i in range(1, ln):
