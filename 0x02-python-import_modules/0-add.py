da56167cb97c388c7a4fec12fd04a02b957a0fe6
nlahkimdev <nlahkimdev@gmail.com>
2023-03-09 16:13:43 -0800

    fix :import function

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+import add_0
 add = __import__('add_0').add
 a = 1
 b = 2
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+print("{:d} + {:d} = {:d}".format(a, b, add_0.add(a, b)))
