f0ad694e717a8d42675ac72f7dc066818177c46a
nlahkimdev <nlahkimdev@gmail.com>
2023-03-09 17:12:00 -0800

    fix :calculator

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from acalculator_1 import add, sub, mul, div
+    from acalculator_1 import add
+    from acalculator_1 import sub
+    from acalculator_1 import mul
+    from acalculator_1 import div
     a = 10
     b = 5
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
