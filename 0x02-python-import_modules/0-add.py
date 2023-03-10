6d8e0f26888885d2ca98325520c0bd5d55aa7fef
nlahkimdev <nlahkimdev@gmail.com>
2023-03-09 17:03:43 -0800

    fix 7:import function

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-import sys
-import os
-
-current = os.path.dirname(os.path.realpath(__file__))
-parent = os.path.dirname(current)
-sys.path.append(parent)
 if __name__ == "__main__":
     from add_0 import add
     a = 1
