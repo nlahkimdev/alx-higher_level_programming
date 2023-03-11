0c572e6917681f4600480cf9d5bf22cbfefe1354
nlahkimdev <nlahkimdev@gmail.com>
2023-03-10 16:13:19 -0800

    fix hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from inspect import getmembers, isfunction
-    import hidden_4 as mod
-    ln = len(getmembers(mod))
-    mem = getmembers(mod)
-    for a in range(0, ln):
-        if isfunction(mem[a][1]):
-            if mem[a][0][0] != '_' and mem[a][0][1] != '_':
-                print(mem[a][0])
+    import sys
+    import hidden_4
+    for i in dir(hidden_4):
+        if i[:2] != "__":
+            print(i)
