dc576bd076f2639b2ab40466eefae8af29c0ef09
nlahkimdev <nlahkimdev@gmail.com>
2023-03-13 08:00:48 -0700

    fix 3: multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -6,7 +6,7 @@ def multiple_returns(sentence):
         result.append(len(sentence))
         result.append(sentence[0])
     elif not sentence:
-        result.append(0)
+        result.append(None)
         result.append("")
     c = tuple(result)
     return c
